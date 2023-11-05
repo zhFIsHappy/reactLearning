2e1c232fa233a109bfb819afb1ce95312608330f
zhfan901 <frank_f@infi.us>
2023-11-04 21:16:50 -0400

    progress

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import styles from './App.css';
+import './App.css';
+import {useState} from 'react';
 
 function App() {
-
-  const planets = [
-    {name : "Mars", isGasPlanet : false},
-    {name : "Earth", isGasPlanet : false},
-    {name : "Jupiter", isGasPlanet : true},
-    {name : "Venus", isGasPlanet : false},   
-  ]
+  const [showText, setShowText] = useState(true);
+  
   return (
     
     <div className="App">
-       {/* 这里使用 planet 这个variable 访问map的 */}
-       {/* 两种方式 */}
-      {planets.map((planet, key) =>{
-        // if(planet.isGasPlanet)return <h1 key = {key}> {planet.name}</h1>;
-        return <h1 key = {key}> {planet.isGasPlanet ? planet.name : ""}</h1>;
-      })}
+      <button onClick={() => {
+         setShowText(!showText);
+        }}>
+          Show/Hide
+      </button>
+
+      {showText && <h1>Show</h1>}
     </div>
   );
 }

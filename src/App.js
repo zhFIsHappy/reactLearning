511ff5989ed66332ce34251b361b00cdf93eba22
zhfan901 <frank_f@infi.us>
2023-11-04 21:21:15 -0400

    quiz passed

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  const [showText, setShowText] = useState(true);
+  const [count, setCount] = useState(0);
   
   return (
     
     <div className="App">
-      <button onClick={() => {
-         setShowText(!showText);
-        }}>
-          Show/Hide
-      </button>
-
-      {showText && <h1>Show</h1>}
+      <button onClick={{} = ()=>{
+        setCount(count + 1);
+      }}>Increase</button>
+      <button onClick={{} = ()=>{
+        setCount(count - 1);
+      }}>Decrease</button>
+      <button onClick={{} = ()=>{
+        setCount(0);
+      }}>Set to Zero</button>
+      {count}
     </div>
   );
 }

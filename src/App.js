ebb6c91982b95dd11f4e9c77f4e19eda0e2fcf6d
zhfan901 <frank_f@infi.us>
2023-11-06 21:05:13 -0500

    cat fact

--- src/App.js
+++ src/App.js
@@ -1,59 +1,29 @@
 import "./App.css";
-import { useState } from "react";
-import { Task } from "./Task";
+import Axios from "axios";
+import { useState, useEffect } from "react";
+// fetch("https://catfact.ninja/fact").then((res) => res.json()).then((data) =>{
+//   console.log(data)
+// });
+
+// fetch data from url, then using the promise to convert it to json, 
+// then use the coverted json "data"
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
-
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
-
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false,
-    };
-    setTodoList(task.taskName !== "" ? [...todoList, task] : todoList);
-  };
-
-  const deleteTask = (id) => {
-    setTodoList(todoList.filter((task) => task.id !== id));
-  };
-
-  const completeTask = (id) => {
-    setTodoList(
-      todoList.map((task) => {
-        if (task.id === id) {
-          return { ...task, completed: true };
-        } else {
-          return task;
-        }
-      })
-    );
-  };
-
+  const [catFact, setCatFact] = useState("");
+  const fetchCatFact = () =>{
+    Axios.get("https://catfact.ninja/fact").then((res) =>{
+      setCatFact(res.data.fact)
+    })
+  }
+  useEffect (() =>{
+    fetchCatFact();
+  }, []);
+  //这里要用 useEffect 只用mount 一次
+  
   return (
     <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange} />
-        <button onClick={addTask}> Add Task</button>
-      </div>
-      <div className="list">
-        {todoList.map((task) => {
-          return (
-            <Task
-              taskName={task.taskName}
-              id={task.id}
-              completed={task.completed}
-              deleteTask={deleteTask}
-              completeTask={completeTask}
-            />
-          );
-        })}
-      </div>
+      <button onClick ={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
     </div>
   );
 }

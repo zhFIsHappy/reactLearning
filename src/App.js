64616c4bd1cbac82b5695c5848921dcbf4252a99
zhfan901 <frank_f@infi.us>
2023-11-06 21:17:56 -0500

    predict_age

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import "./App.css";
 import Axios from "axios";
 import { useState, useEffect } from "react";
-// fetch("https://catfact.ninja/fact").then((res) => res.json()).then((data) =>{
-//   console.log(data)
-// });
-
-// fetch data from url, then using the promise to convert it to json, 
-// then use the coverted json "data"
 
 function App() {
-  const [catFact, setCatFact] = useState("");
-  const fetchCatFact = () =>{
-    Axios.get("https://catfact.ninja/fact").then((res) =>{
-      setCatFact(res.data.fact)
-    })
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null)
+  const fetchData = () =>{
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => { setPredictedAge(res.data.age)})
   }
-  useEffect (() =>{
-    fetchCatFact();
-  }, []);
-  //这里要用 useEffect 只用mount 一次
-  
   return (
     <div className="App">
-      <button onClick ={fetchCatFact}>Generate Cat Fact</button>
-      <p>{catFact}</p>
+      <input placeholder="input your name" onChange = {(event) =>{setName(event.target.value)}}></input>
+      <button onClick={fetchData}>Predict Age</button>
+      <h1>predict your age: {predictedAge?.age} </h1>
     </div>
   );
 }
